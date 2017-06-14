4910cb1ce237af8509a0bd9d6fa8547c74f66cc5
UladKasach <nappa300@gmail.com>
2017-06-14 11:02:35 -0400

    #1 - insert iframe parent height messaging script into header

--- public/views/symptom/header.php
+++ public/views/symptom/header.php
@@ -7,3 +7,19 @@
 	<span id="header_text">Information, Resources, and Tools for People Dealing with Cancer</span>
 	</div>
 </header>  -->
+
+<script>
+window.addEventListener("load", function(){
+    if(window.self === window.top) return; // if w.self === w.top, we are not in an iframe 
+    send_height_to_parent_function = function(){
+        var height = document.getElementsByTagName("html")[0].clientHeight;
+        //console.log("Sending height as " + height + "px");
+        parent.postMessage({"height" : height }, "*");
+    }
+    // send message to parent about height updates
+    window.addEventListener("resize", send_height_to_parent_function); // whenever the page is resized
+    var observer = new MutationObserver(send_height_to_parent_function);           // whenever DOM changes PT1
+    var config = { attributes: true, childList: true, characterData: true, subtree:true}; // PT2
+    observer.observe(window.document, config);                                            // PT3 
+});
+</script>
